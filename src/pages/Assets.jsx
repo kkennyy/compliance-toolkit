28856de4271441661c72379131a9a8f0b144ef73
kkennyy <84776380+kkennyy@users.noreply.github.com>
2024-12-10 14:56:34 +0800

    Update Assets.jsx

--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import AssetList from '../components/assets/AssetList';
 import AssetDetail from '../components/assets/AssetDetail';
 
 const Assets = () => {
+  const location = useLocation();
+  console.log('Assets component rendering, path:', location.pathname);
+
   return (
     <Routes>
-      <Route path="/" element={<AssetList />} />
-      <Route path="/:id/*" element={<AssetDetail />} />
+      <Route index element={<AssetList />} />
+      <Route path=":id" element={<AssetDetail />} />
     </Routes>
   );
 };
