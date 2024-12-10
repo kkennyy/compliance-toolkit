3a9a1b6ea21b5a2a15ea1f0effd2eda2ee399824
kkennyy <84776380+kkennyy@users.noreply.github.com>
2024-12-10 14:54:55 +0800

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,8 +4,9 @@ import { AuthContext } from '../contexts/AuthContext';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  
+  console.log('useAuth context:', context); // Add this line
   if (!context) {
+    console.error('Auth context is null!'); // Add this line
     throw new Error('useAuth must be used within an AuthProvider');
   }
   
