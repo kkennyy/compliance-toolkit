45176a5e5cd3f44eaeb8051f0f8136acc6585449
kkennyy <84776380+kkennyy@users.noreply.github.com>
2024-12-10 11:43:50 +0800

    Create AuthContext.jsx

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { supabase } from '../config/supabaseClient';
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const value = {
+    session,
+    loading,
+    signUp: (data) => supabase.auth.signUp(data),
+    signIn: (data) => supabase.auth.signInWithPassword(data),
+    signOut: () => supabase.auth.signOut(),
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
